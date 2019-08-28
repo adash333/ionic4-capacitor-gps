df880bba33a59596caaac53996fe86183283d5cd
adash333 <adash333@hotmail.com>
2019-08-28 23:12:22 +0900

    A2HS Button

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Geolocation} from '@capacitor/core';
+import { Geolocation } from '@capacitor/core';
 
 @Component({
   selector: 'app-tab1',
@@ -7,9 +7,11 @@ import { Geolocation} from '@capacitor/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
   latitude: number;
   longitude: number;
+
+  showBtn: boolean = false;
+  deferredPrompt;
 
   constructor() {
     this.getLocation();
@@ -19,5 +21,43 @@ export class Tab1Page {
     const position = await Geolocation.getCurrentPosition();
     this.latitude = position.coords.latitude;
     this.longitude = position.coords.longitude;
+  }
+
+  ngOnInit() {
+    window.addEventListener('beforeinstallprompt', e => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later on the button event.
+      this.deferredPrompt = e;
+
+      // Update UI by showing a button to notify the user they can add to home screen
+      this.showBtn = true;
+    });
+
+    //button click event to show the promt
+
+    window.addEventListener('appinstalled', event => {
+      alert('installed');
+    });
+
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+      alert('display-mode is standalone');
+    }
+  }
+
+  add_to_home(e) {
+    debugger;
+    // hide our user interface that shows our button
+    // Show the prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === 'accepted') {
+        alert('User accepted the prompt');
+      } else {
+        alert('User dismissed the prompt');
+      }
+      this.deferredPrompt = null;
+    });
   }
 }
